f1ccdf755424150b110eba7b4b48330b96074f00
bek77g <d.zheenkul@gmail.com>
2023-10-08 01:41:10 +0600

    try: fix load

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./index.css";
-import "./modal";
+// import "./modal";
 
 import config from "./config.json";
 import { flamesArray, videosObj } from "./constants";
@@ -11,31 +11,33 @@ import {
   getVideoEntity,
 } from "./utils";
 
-// eslint-disable-next-line no-undef
-AFRAME.registerComponent("birthday-party", {
-  init() {
-    const sceneEl = document.querySelector("a-scene");
-    const cakeEl = sceneEl.querySelector("#cake-parent");
-    flamesArray.forEach((flame) => {
-      const flameEntity = getFlameEntity(flame);
-      cakeEl.appendChild(flameEntity);
-      return flame;
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // eslint-disable-next-line no-undef
+  AFRAME.registerComponent("birthday-party", {
+    init() {
+      const sceneEl = document.querySelector("a-scene");
+      const cakeEl = sceneEl.querySelector("#cake-parent");
+      flamesArray.forEach((flame) => {
+        const flameEntity = getFlameEntity(flame);
+        cakeEl.appendChild(flameEntity);
+        return flame;
+      });
 
-    const tvContainerEl = sceneEl.querySelector("#tv-container");
+      const tvContainerEl = sceneEl.querySelector("#tv-container");
 
-    Object.keys(videosObj).forEach((key) => {
-      videosObj[key] = { ...videosObj[key], ...config.videosObj[key] };
-    });
+      Object.keys(videosObj).forEach((key) => {
+        videosObj[key] = { ...videosObj[key], ...config.videosObj[key] };
+      });
 
-    Object.keys(videosObj).forEach((key) => {
-      const videoEntity = getVideoEntity(videosObj[key], `video-${key}`);
-      tvContainerEl.appendChild(videoEntity);
-    });
+      Object.keys(videosObj).forEach((key) => {
+        const videoEntity = getVideoEntity(videosObj[key], `video-${key}`);
+        tvContainerEl.appendChild(videoEntity);
+      });
 
-    // const textEl = getTextEntity(config.name);
-    // sceneEl.appendChild(textEl);
-    const ageEl = getAgeEntity(config.age);
-    sceneEl.appendChild(ageEl);
-  },
+      // const textEl = getTextEntity(config.name);
+      // sceneEl.appendChild(textEl);
+      const ageEl = getAgeEntity(config.age);
+      sceneEl.appendChild(ageEl);
+    },
+  });
 });
